2453b1e381c762e5e80b39fa662296c04593e03d
Karandeep Sandhu <ksandhu1054@conestogac.on.ca>
2026-01-06 13:16:20 -0500

    Add supabase type

--- youtube-video-bias-checker/apps/biasMateExension/lib/supabase.ts
+++ youtube-video-bias-checker/apps/biasMateExension/lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import { Database } from "../../web/src/types/database.types";
+import { Database } from "../utils/database.types";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
